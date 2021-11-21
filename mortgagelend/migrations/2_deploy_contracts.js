e427d534549ad63f181b7055a729182153b8d4df
jordan <jordan@bitola.com>
2021-11-21 13:45:38 -0500

    Ropsten deployments

---
+++ mortgagelend/migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+var Pawn = artifacts.require("Pawn");
+
+// accounts[0] = owner (of all contracts for testing purposes)
+module.exports = async function(deployer, network, accounts) {
+  await deployer.deploy(Pawn);
+  
+  console.log("Pawn: " + Pawn.address);
+  console.log("Owner: "+accounts[0]);
+  console.log("Demployment completed!");
+};
